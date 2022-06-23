b65739673fa0d0113dc0768322112e96069b4547
MelihDemirtel <melihcandemirtel@gmail.com>
2022-06-23 21:37:09 +0300

    base code is ok and run chrome driver and open youtube url

--- Introduction/src/SeleniumIntroduction.java
+++ Introduction/src/SeleniumIntroduction.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntroduction {
 
@@ -5,6 +7,16 @@ public class SeleniumIntroduction {
 
 		// Launch Chrome Diver
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		//Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Screen Manage
+		driver.manage().window().maximize();
+
+		// GET Main URL
+		driver.get("https://www.youtube.com/");
+		System.out.println("LOG - URL is Opened");
 
 
 	}
