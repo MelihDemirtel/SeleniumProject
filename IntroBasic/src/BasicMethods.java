4efc964de395f7287f058cc790710f9869011c76
MelihDemirtel <melihcandemirtel@gmail.com>
2022-06-24 17:00:42 +0300

    create basicMethods java class

--- IntroBasic/src/BasicMethods.java
+++ IntroBasic/src/BasicMethods.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumIntroduction {
+public class BasicMethods {
 
 	public static void main(String[] args) {
 
@@ -15,7 +15,7 @@ public class SeleniumIntroduction {
 		driver.manage().window().maximize();
 
 		// GET Main URL
-		driver.get("https://wahulshettyacademy.com");
+		driver.get("https://rahulshettyacademy.com");
 		System.out.println("LOG - URL is Opened");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
