f5bbae1725cef7ac8cd43f339908ca53645d81c8
MelihDemirtel <melihcandemirtel@gmail.com>
2022-06-26 10:43:43 +0300

    navigateMethods project is done

--- IntroBasic/src/DriverNavigateMethods.java
+++ IntroBasic/src/DriverNavigateMethods.java
@@ -1,8 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverNavigateMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Launch Chrome Diver
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+				
+		//Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Goes To The URL We Typed
+		driver.get("https://www.amazon.com");
+		
+		// Goes To The URL We Typed And Allows Forward And Back Operations
+		driver.navigate().to("https://www.facebook.com");
+		
+		// Back Operation
+		driver.navigate().back();
+		
+		// Forward Operation
+		driver.navigate().forward();
+		
+		// Refresh Current Page
+		driver.navigate().refresh();
+		
+		// Close Driver One Tab Or One Window
+		driver.close();
+		// Close Driver All Tabs And Windows
+		//driver.quit();
 
 	}
 
