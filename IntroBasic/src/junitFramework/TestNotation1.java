bc08562e4d4a86a4d83233df2d1c9586f73b257b
MelihDemirtel <melihcandemirtel@gmail.com>
2022-06-26 15:06:38 +0300

    testNotation1 project is done

--- IntroBasic/src/junitFramework/TestNotation1.java
+++ IntroBasic/src/junitFramework/TestNotation1.java
@@ -1,5 +1,47 @@
 package junitFramework;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestNotation1 {
+	
+	@Test
+	public void test01() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		//Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Screen Manage
+		driver.manage().window().maximize();
+		
+		// Wait 15 Seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		//Wait 15 seconds for the web elements on the page to be found. If it finds the element, it continues without waiting.
+		//Throws an error if it doesn't find the element within 15 seconds
+		
+		// Goes To Amazon Home Page
+		driver.get("https://www.amazon.com");
+				
+		// Create A Web Element
+		WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+		
+		// Send To Search Box Nutella Text
+		searchBox.sendKeys("Nutella");
+				
+		// Click Enter
+		searchBox.submit();
+				
+		// Create A Web Element
+		WebElement resultsText = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+				
+		// Print Search Result
+		System.out.println(resultsText.getText());
+	}
 
 }
---
+++ IntroBasic/src/junitFramework/TestNotation2.java
@@ -0,0 +1,9 @@
+package junitFramework;
+
+public class TestNotation2 {
+	
+	public void test01() {
+		
+	}
+
+}
