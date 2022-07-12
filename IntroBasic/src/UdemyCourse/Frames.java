37175f1b493b2c9107b29ae2c0259b33f043ef61
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-12 23:05:12 +0300

    frames is over

--- IntroBasic/src/UdemyCourse/Frames.java
+++ IntroBasic/src/UdemyCourse/Frames.java
@@ -1,9 +1,42 @@
 package UdemyCourse;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Frames {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		// Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Screen Manage
+		driver.manage().window().maximize();
+
+		// Wait 15 Seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		// Wait 15 seconds for the web elements on the page to be found. If it finds the
+		// element, it continues without waiting.
+		// Throws an error if it doesn't find the element within 15 seconds
+
+		driver.get("https://jqueryui.com/droppable/");
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+		
+		WebElement drag = driver.findElement(By.id("draggable"));
+		
+		WebElement drop = driver.findElement(By.id("droppable"));
+		
+		Actions actions = new Actions(driver);
+		
+		actions.dragAndDrop(drag, drop).build().perform();
 
 	}
 
