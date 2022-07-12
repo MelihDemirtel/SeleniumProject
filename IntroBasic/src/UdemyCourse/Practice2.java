78a40cd90d34d01ae776c924382d9848d258db24
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-12 22:41:17 +0300

    practice2 project created

---
+++ IntroBasic/src/UdemyCourse/Practice2.java
@@ -0,0 +1,33 @@
+package UdemyCourse;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		int second = 1000;
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		// Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Screen Manage
+		driver.manage().window().maximize();
+
+		// Wait 15 Seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		// Wait 15 seconds for the web elements on the page to be found. If it finds the
+		// element, it continues without waiting.
+		// Throws an error if it doesn't find the element within 15 seconds
+
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		driver.findElement(By.linkText("Multiple Windows")).click();
+	}
+
+}
