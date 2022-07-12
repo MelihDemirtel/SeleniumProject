cb577f6948b0e903030fb246c0aa1de3a5de4cdc
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-12 22:52:24 +0300

    practice2 is over, frames project created

--- IntroBasic/src/UdemyCourse/Practice2.java
+++ IntroBasic/src/UdemyCourse/Practice2.java
@@ -1,15 +1,20 @@
 package UdemyCourse;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Practice2 {
 
 	public static void main(String[] args) {
-		int second = 1000;
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
@@ -28,6 +33,23 @@ public class Practice2 {
 		driver.get("https://the-internet.herokuapp.com/");
 		
 		driver.findElement(By.linkText("Multiple Windows")).click();
+		
+		driver.findElement(By.linkText("Click Here")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		
+		String parentId = it.next();
+		String childId = it.next();
+		
+		driver.switchTo().window(parentId);
+		
+		WebElement text = driver.findElement(By.xpath("//h3[text()='Opening a new window']"));
+		
+		System.out.println(text.getText());
+		
+		Assert.assertEquals("Opening a new window", text.getText());
+
 	}
 
 }
