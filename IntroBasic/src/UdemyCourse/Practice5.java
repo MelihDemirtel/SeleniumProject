bc165080900953b346d15d6b6b7bc96c01a91384
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-19 16:58:48 +0300

    practice5 is over

--- IntroBasic/src/UdemyCourse/Practice5.java
+++ IntroBasic/src/UdemyCourse/Practice5.java
@@ -1,14 +1,18 @@
 package UdemyCourse;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Practice5 {
 
 	public static void main(String[] args) throws InterruptedException {
-		int second = 2000;
+		int second = 500;
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
@@ -24,8 +28,22 @@ public class Practice5 {
 		// element, it continues without waiting.
 		// Throws an error if it doesn't find the element within 15 seconds
 
-		driver.get("https://qaclickacademy.com");
+		driver.get("https://qaclickacademy.com/practice.php");
 		Thread.sleep(second);
+		
+		driver.findElement(By.id("autocomplete")).sendKeys("ind");
+		Thread.sleep(second);
+		
+		List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']"));
+		
+		Actions actions = new Actions(driver);
+		
+		for(WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("India")) {
+				actions.moveToElement(option).click().build().perform();
+				break;
+			}
+		}
 
 	}
 
