a5a3ae5c09e43e663314f287f3d2bc29ef2ce9ae
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-19 15:55:27 +0300

    scrolling is over

--- IntroBasic/src/UdemyCourse/Scrolling.java
+++ IntroBasic/src/UdemyCourse/Scrolling.java
@@ -2,25 +2,35 @@ package UdemyCourse;
 
 import java.time.Duration;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scrolling {
-	int second = 1000;
-	System.setProperty("webdriver.chrome.driver",
-			"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	public static void main(String[] args) throws InterruptedException {
+		int second = 1000;
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
-	// Create Chrome Driver Object
-	WebDriver driver = new ChromeDriver();
+		// Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
 
-	// Screen Manage
-	driver.manage().window().maximize();
+		// Screen Manage
+		driver.manage().window().maximize();
 
-	// Wait 15 Seconds
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-	// Wait 15 seconds for the web elements on the page to be found. If it finds the
-	// element, it continues without waiting.
-	// Throws an error if it doesn't find the element within 15 seconds
+		// Wait 15 Seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		// Wait 15 seconds for the web elements on the page to be found. If it finds the
+		// element, it continues without waiting.
+		// Throws an error if it doesn't find the element within 15 seconds
 
-	driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(3000);
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=500");
+
+	}
 }
