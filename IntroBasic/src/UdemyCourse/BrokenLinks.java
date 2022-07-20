d1c0485a1385659063778946b06b29efe3d2eb87
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-20 11:33:44 +0300

    brokenLinks project continue

--- IntroBasic/src/UdemyCourse/BrokenLinks.java
+++ IntroBasic/src/UdemyCourse/BrokenLinks.java
@@ -1,13 +1,18 @@
 package UdemyCourse;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrokenLinks {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException, IOException {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
 		//Create Chrome Driver Object
@@ -22,7 +27,16 @@ public class BrokenLinks {
 		//Throws an error if it doesn't find the element within 15 seconds
 		
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-
+		
+		String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");//a[href*='brokenlink']
+		
+		HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+		
+		conn.setRequestMethod("HEAD");
+		conn.connect();
+		int responseCode = conn.getResponseCode();
+		System.out.println(responseCode);
+		
 	}
 
 }
