403df69537117d3c69176a80b310e63d6cac2d75
MelihDemirtel <melihcandemirtel@gmail.com>
2022-07-23 12:44:09 +0300

    brokenLinksSoftAssert project created

---
+++ IntroBasic/src/UdemyCourse/BrokenLinksSoftAssert.java
@@ -0,0 +1,46 @@
+package UdemyCourse;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinksSoftAssert {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ext02d47194\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		//Create Chrome Driver Object
+		WebDriver driver = new ChromeDriver();
+
+		// Screen Manage
+		driver.manage().window().maximize();
+		
+		// Wait 15 Seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		//Wait 15 seconds for the web elements on the page to be found. If it finds the element, it continues without waiting.
+		//Throws an error if it doesn't find the element within 15 seconds
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");// a[href*='brokenlink'] // a[href*='soapui']
+		
+		HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+		
+		conn.setRequestMethod("HEAD");
+		conn.connect();
+		int responseCode = conn.getResponseCode();
+		System.out.println("Code : "+responseCode);
+		if(responseCode > 400)
+		{
+			System.out.println("Broken Link : "+url);
+			Assert.assertTrue(false);
+		}
+
+	}
+
+}
